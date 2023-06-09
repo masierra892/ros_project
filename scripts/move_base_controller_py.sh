00f8ec7119c8d5ff2082be77745425abf1751278
masierra892 <85864107+masierra892@users.noreply.github.com>
2023-06-09 10:27:11 +0900

    Update directory var in Bash File

--- scripts/move_base_controller_py.sh
+++ scripts/move_base_controller_py.sh
@@ -8,6 +8,6 @@ source devel/setup.bash
 
 export TURTLEBOT3_MODEL=burger
 
-project_directory=$( echo "$PWD" )
+project_directory=$script_path/catkin_ws/src/ros_project
 
 roslaunch project_launcher project_launcher_move_base_py.launch map_path:=$project_directory/map/map.yaml
